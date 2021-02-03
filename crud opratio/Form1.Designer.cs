23e3a362b7beabad84611e3013da7059de23bf36
vickyvc <42885511+vickyvc@users.noreply.github.com>
2021-02-03 14:28:04 +0530

    Add files via upload

---
+++ crud opratio/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace crud_opratio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_pid = new System.Windows.Forms.TextBox();
+            this.txt_pname = new System.Windows.Forms.TextBox();
+            this.txt_cid = new System.Windows.Forms.TextBox();
+            this.txt_cname = new System.Windows.Forms.TextBox();
+            this.btn_create = new System.Windows.Forms.Button();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.bt_del = new System.Windows.Forms.Button();
+            this.gridview = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Category ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Category Name";
+            // 
+            // txt_pid
+            // 
+            this.txt_pid.Location = new System.Drawing.Point(98, 22);
+            this.txt_pid.Name = "txt_pid";
+            this.txt_pid.Size = new System.Drawing.Size(100, 20);
+            this.txt_pid.TabIndex = 4;
+            // 
+            // txt_pname
+            // 
+            this.txt_pname.Location = new System.Drawing.Point(98, 50);
+            this.txt_pname.Name = "txt_pname";
+            this.txt_pname.Size = new System.Drawing.Size(100, 20);
+            this.txt_pname.TabIndex = 5;
+            // 
+            // txt_cid
+            // 
+            this.txt_cid.Location = new System.Drawing.Point(98, 76);
+            this.txt_cid.Name = "txt_cid";
+            this.txt_cid.Size = new System.Drawing.Size(100, 20);
+            this.txt_cid.TabIndex = 6;
+            // 
+            // txt_cname
+            // 
+            this.txt_cname.Location = new System.Drawing.Point(98, 102);
+            this.txt_cname.Name = "txt_cname";
+            this.txt_cname.Size = new System.Drawing.Size(100, 20);
+            this.txt_cname.TabIndex = 7;
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(12, 149);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(75, 23);
+            this.btn_create.TabIndex = 8;
+            this.btn_create.Text = "Create";
+            this.btn_create.UseVisualStyleBackColor = true;
+            this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
+            // 
+            // btn_read
+            // 
+            this.btn_read.Location = new System.Drawing.Point(93, 149);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(75, 23);
+            this.btn_read.TabIndex = 9;
+            this.btn_read.Text = "Read";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(174, 149);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(75, 23);
+            this.btn_update.TabIndex = 10;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // bt_del
+            // 
+            this.bt_del.Location = new System.Drawing.Point(264, 149);
+            this.bt_del.Name = "bt_del";
+            this.bt_del.Size = new System.Drawing.Size(75, 23);
+            this.bt_del.TabIndex = 11;
+            this.bt_del.Text = "Delete";
+            this.bt_del.UseVisualStyleBackColor = true;
+            this.bt_del.Click += new System.EventHandler(this.bt_del_Click);
+            // 
+            // gridview
+            // 
+            this.gridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridview.Location = new System.Drawing.Point(379, 22);
+            this.gridview.Name = "gridview";
+            this.gridview.Size = new System.Drawing.Size(240, 184);
+            this.gridview.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 425);
+            this.Controls.Add(this.gridview);
+            this.Controls.Add(this.bt_del);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.btn_create);
+            this.Controls.Add(this.txt_cname);
+            this.Controls.Add(this.txt_cid);
+            this.Controls.Add(this.txt_pname);
+            this.Controls.Add(this.txt_pid);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_pid;
+        private System.Windows.Forms.TextBox txt_pname;
+        private System.Windows.Forms.TextBox txt_cid;
+        private System.Windows.Forms.TextBox txt_cname;
+        private System.Windows.Forms.Button btn_create;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.Button bt_del;
+        private System.Windows.Forms.DataGridView gridview;
+    }
+}
+
---
+++ crud opratio/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace crud_opratio
+{
+    public partial class Form1 : Form
+    {
+        public SqlConnection sc = new SqlConnection();
+        public SqlCommand cmd = new SqlCommand();
+        public SqlDataReader dr;
+        SqlDataAdapter adp;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_create_Click(object sender, EventArgs e)
+        {
+            sc.ConnectionString = "database=crud1;server=DESKTOP-8KEM77V\\SQLEXPRESS;integrated security=true";
+            cmd = new SqlCommand("insert into tb_1(txt_pid,txt_pname,txt_cid,txt_cname) Values (@txt_pid,@txt_pname,@txt_cid,@txt_cname)", sc);
+            sc.Open();
+            cmd.Parameters.AddWithValue("@txt_pid", txt_pid.Text);
+            cmd.Parameters.AddWithValue("@txt_pname", txt_pname.Text);
+            cmd.Parameters.AddWithValue("@txt_cid", txt_cid.Text);
+            cmd.Parameters.AddWithValue("@txt_cname", txt_cname.Text);
+
+            cmd.ExecuteNonQuery();
+            sc.Close();
+            MessageBox.Show("DATA indseted sucessfully");
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            sc.ConnectionString = "database=crud1;server=DESKTOP-8KEM77V\\SQLEXPRESS;integrated security=true";
+            sc.Open();
+
+            DataTable dt = new DataTable();
+            adp = new SqlDataAdapter("select * from tb_1", sc);
+            adp.Fill(dt);
+            gridview.DataSource = dt;
+            sc.Close();
+        }
+
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            sc.ConnectionString = "database=crud1;server=DESKTOP-8KEM77V\\SQLEXPRESS;integrated security=true";
+            cmd = new SqlCommand("update tb_1 set txt_pname=@txt_pname,txt_cid=@txt_cid,txt_cname=@txt_cname WHERE txt_pid = @txt_pid", sc);
+            sc.Open();
+            cmd.Parameters.AddWithValue("@txt_pid", txt_pid.Text);
+            cmd.Parameters.AddWithValue("@txt_pname", txt_pname.Text);
+            cmd.Parameters.AddWithValue("@txt_cid", txt_cid.Text);
+            cmd.Parameters.AddWithValue("@txt_cname", txt_cname.Text);
+
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("DATA UPDATED sucessfully");
+            sc.Close();
+          
+
+        }
+
+        private void bt_del_Click(object sender, EventArgs e)
+        {
+            sc.ConnectionString = "database=crud1;server=DESKTOP-8KEM77V\\SQLEXPRESS;integrated security=true";
+            cmd = new SqlCommand("delete tb_1 where txt_pid=@txt_pid", sc);
+            sc.Open();
+
+            cmd.Parameters.AddWithValue("@txt_pid", txt_pid.Text);
+            cmd.ExecuteNonQuery();
+            sc.Close();
+            MessageBox.Show("Data deleted successfully");
+
+        }
+    }
+}
